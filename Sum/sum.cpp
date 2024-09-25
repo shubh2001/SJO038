341b52c05649828c8705f6758ce2fc722fb55681
shubh2001 <shubhsinghal02@gmail.com>
2024-09-25 11:30:24 +0200

    Version 03 Sum

--- Sum/sum.cpp
+++ Sum/sum.cpp
@@ -2,17 +2,22 @@
 #include <string>
 using namespace std;
 
+
+int getSum(int chosenNum){
+    int sum = 0;
+    for (int i=1; i<=chosenNum; ++i){
+        sum+=i;
+    }
+    return sum;
+}
+
 int main()
 {
     int usernum;
     cout << "Enter a number greater than 1\n";
     cin >> usernum;
-
-
-    int sum = 0;
-    for (int i = 1; i <= usernum; ++i){
-        sum += i;
-    }
+    
+    int sum = getSum(usernum);
 
     cout << "Sum of integers from 1 to " << usernum << " is: " <<  sum << "\n";
     return 0;
