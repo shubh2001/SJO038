0ef3d47632b840eed5f17dadb22eb631848facd2
shubh2001 <shubhsinghal02@gmail.com>
2024-10-04 19:10:56 +0200

    Power of 2 Version 03

--- MathExercise/divbytwo.cpp
+++ MathExercise/divbytwo.cpp
@@ -5,6 +5,9 @@ bool DivByTwo(int n) {
     if (n == 1){  
         return true;
     }
+    if (n==0){
+        return false;
+    }
     while(n > 1){
         if(n % 2 != 0) {  
             return false;
