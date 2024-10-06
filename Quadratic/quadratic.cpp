db6c3da79ee2706fde5f9afebafea4b16711623c
shubh2001 <shubhsinghal02@gmail.com>
2024-10-06 22:48:26 +0200

    Quadratic Version 02

--- Quadratic/quadratic.cpp
+++ Quadratic/quadratic.cpp
@@ -2,6 +2,21 @@
 #include <cmath>
 using namespace std;
 
+
+
+int quadsolver(int a, int b, int c){
+    double x1;
+    double x2;
+    double discriminant = sqrt(b*b - 4*a*c);
+
+    if(discriminant >= 0){
+     x1 = (-b + sqrt(b*b - 4*a*c))/(2*a);
+     x2 = (-b - sqrt(b*b - 4*a*c))/(2*a);
+    }
+
+    cout << "The solutions are " << x1 << " and " << x2 << "\n"; 
+    return 0;
+}
 int main(){
     int a;
     int b;
@@ -16,9 +31,5 @@ int main(){
     cout << "Enter value of c: \n";
     cin >> c;
 
-    double x1 = (-b + sqrt(b*b - 4*a*c))/(2*a);
-    double x2 = (-b - sqrt(b*b - 4*a*c))/(2*a);
-
-   
-    cout << "The solutions are " << x1 << " and " << x2 << "\n"; 
+    quadsolver(a,b,c);
 }
