6941540041a136b54ea7ed38695f1e529413a8dd
shubh2001 <shubhsinghal02@gmail.com>
2024-10-13 21:11:47 +0200

    Version 05

--- ros2_ws/src/sum/src/sum.cpp
+++ ros2_ws/src/sum/src/sum.cpp
@@ -4,11 +4,14 @@
 
 
 int sum;
+std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Int32>>publisher;
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {   
     sum += msg->data;
-    std::cout << sum << std::endl;
+    std_msgs::msg::Int32 out_msg;
+    out_msg.data = sum;
+    publisher->publish(out_msg);
 }
 
 int main(int argc, char * argv[])
@@ -18,7 +21,8 @@ int main(int argc, char * argv[])
     auto node = rclcpp::Node::make_shared("sum");
     auto subscription = 
     node -> create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
-
+ 
+    publisher = node -> create_publisher<std_msgs::msg::Int32>("sum", 10);
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
