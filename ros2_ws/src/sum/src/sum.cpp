cd5238af75556e963b6a028eab48643cb815b84b
shubh2001 <shubhsinghal02@gmail.com>
2024-10-13 20:39:24 +0200

    Int32 msg type

--- ros2_ws/src/sum/src/sum.cpp
+++ ros2_ws/src/sum/src/sum.cpp
@@ -1,18 +1,18 @@
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/int32.hpp"
 #include <iostream>
 
-void topic_callback(const std_msgs::msg::String::SharedPtr msg)
+void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {
     std::cout << msg->data << std::endl;
 }
 
 int main(int argc, char * argv[])
-{
+{   
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("subscriber");
+    auto node = rclcpp::Node::make_shared("sum");
     auto subscription = 
-    node -> create_subscription<std_msgs::msg::String>("topic", 10, topic_callback);
+    node -> create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
 
     rclcpp::spin(node);
     rclcpp::shutdown();
