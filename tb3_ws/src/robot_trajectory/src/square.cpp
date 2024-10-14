45edc76fc05526324ca8e89c9ba1c7669a9d8eed
shubh2001 <shubhsinghal02@gmail.com>
2024-10-14 21:36:06 +0200

    Added Speed parameter

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -10,57 +10,56 @@ int main(int argc, char * argv[])
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("publisher");
     auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel",10);
+    node->declare_parameter("speed", 0.1);
     geometry_msgs::msg::Twist message;
     rclcpp::WallRate loop_rate(10ms);
 
-    // while (rclcpp::ok())
-    // {   
-    //     message.linear.x = 1.0;
-    //     message.angular.z = 1.0;
-    //     publisher->publish(message);
-    //     rclcpp::spin_some(node);
-    //     loop_rate.sleep();
-    // }
 
-    int i=0, n=1000;
-    while (rclcpp::ok() && i<n)
+    double speed = node->get_parameter("speed").get_parameter_value().get<double>();
+    for(int j=0; j<4; j++)
     {
-        i++;
-        message.linear.x = 0.1;
+        int i=0, n=1000;
+        while (rclcpp::ok() && i<n)
+        {
+            i++;
+            message.linear.x = speed;
+            publisher->publish(message);
+            rclcpp::spin_some(node);
+            loop_rate.sleep();
+        }
+
+        message.linear.x = 0;
+        message.linear.y = 0;
+        message.linear.z = 0;
+
+        message.angular.x = 0;
+        message.angular.y = 0;
+        message.angular.z = 0;
+
         publisher->publish(message);
-        rclcpp::spin_some(node);
-        loop_rate.sleep();
+
+        i=0, n=1000;
+        while (rclcpp::ok() && i<n)
+        {
+            i++;
+            message.angular.z = (9.0 * 3.14)/180;
+            publisher->publish(message);
+            rclcpp::spin_some(node);
+            loop_rate.sleep();
+        }
+
+        message.linear.x = 0;
+        message.linear.y = 0;
+        message.linear.z = 0;
+
+        message.angular.x = 0;
+        message.angular.y = 0;
+        message.angular.z = 0;
+
+        publisher->publish(message);
     }
 
-    message.linear.x = 0;
-    message.linear.y = 0;
-    message.linear.z = 0;
 
-    message.angular.x = 0;
-    message.angular.y = 0;
-    message.angular.z = 0;
-
-    publisher->publish(message);
-
-    i=0, n=1000;
-    while (rclcpp::ok() && i<n)
-    {
-        i++;
-        message.angular.z = (9.0 * 3.14)/180;
-        publisher->publish(message);
-        rclcpp::spin_some(node);
-        loop_rate.sleep();
-    }
-
-    message.linear.x = 0;
-    message.linear.y = 0;
-    message.linear.z = 0;
-
-    message.angular.x = 0;
-    message.angular.y = 0;
-    message.angular.z = 0;
-
-    publisher->publish(message);
 
     rclcpp::shutdown();
     return 0;
