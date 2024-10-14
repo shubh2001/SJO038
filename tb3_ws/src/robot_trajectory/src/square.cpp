3253b901330b181f377f644811c6b8ba05e5fd01
shubh2001 <shubhsinghal02@gmail.com>
2024-10-14 17:41:12 +0200

    Changed Topic Name and Message Type

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -1,6 +1,6 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
 using namespace std::chrono_literals;
 
@@ -9,14 +9,12 @@ int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("publisher");
-    auto publisher = node->create_publisher<std_msgs::msg::String>("topic",10);
-    std_msgs::msg::String message;
-    auto publish_count = 0 ;
+    auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel",10);
+    geometry_msgs::msg::Twist message;
     rclcpp::WallRate loop_rate(500ms);
 
     while (rclcpp::ok())
     {
-        message.data = "Hello, world!" + std::to_string(publish_count++);
         publisher->publish(message);
         rclcpp::spin_some(node);
         loop_rate.sleep();
