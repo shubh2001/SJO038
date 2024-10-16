08e6f66efa36189cec223c6d10535f6fff3afd66
shubh2001 <shubhsinghal02@gmail.com>
2024-10-16 16:22:32 +0200

    Added Float64 msg type to fix mean

--- ros2_ws/src/stats/src/mean.cpp
+++ ros2_ws/src/stats/src/mean.cpp
@@ -1,30 +1,33 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
+#include "example_interfaces/msg/float64.hpp" 
 #include <iostream>
 
 
-int sum;
-int msg_count=0;
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Int32>>publisher;
+double sum;
+int msg_count=0.0;
+double mean=0.0;
+std::shared_ptr<rclcpp::Publisher<example_interfaces::msg::Float64>> publisher;
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {   
     sum += msg->data;
     msg_count++;
-    std_msgs::msg::Int32 out_msg;
-    out_msg.data = sum/msg_count;
+    example_interfaces::msg::Float64 out_msg;
+    mean = sum/msg_count;
+    out_msg.data = mean;
     publisher->publish(out_msg);
 }
 
 int main(int argc, char * argv[])
 {   
-    sum = 0;
+    sum = 0.0;
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("mean");
     auto subscription = 
     node -> create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
  
-    publisher = node -> create_publisher<std_msgs::msg::Int32>("mean", 10);
+    publisher = node -> create_publisher<example_interfaces::msg::Float64>("mean", 10);
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
